9995752197b91e0d4dc502bef8a9ca84c9142a00
ilazarkov <ilazarkov@gmail.com>
2026-03-23 11:41:12 -0500

    improved identation
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>

--- src/__tests__/ui.test.tsx
+++ src/__tests__/ui.test.tsx
@@ -4,28 +4,28 @@ import "@testing-library/jest-dom";
 
 describe("App Component UI Tests", () => {
 
-  test("renders the 9x9 Sudoku board", async () => {
-    const container = await act(() => {
-      const  {container} = render(<App />);
-      return container;
+    test("renders the 9x9 Sudoku board", async () => {
+        const container = await act(() => {
+            const { container } = render(<App />);
+            return container;
+        });
+
+        // const { container } = render(<App />);
+        // Since each cell is a span inside the board
+        const cells = container.querySelectorAll("main span");
+        expect(cells.length).toBe(81);
     });
 
-    // const { container } = render(<App />);
-    // Since each cell is a span inside the board
-    const cells = container.querySelectorAll("main span");
-    expect(cells.length).toBe(81);
-  });
+    test("renders the digit input buttons (1-9)", async () => {
+        await act(() => {
+            render(<App />);
+        });
 
-  test("renders the digit input buttons (1-9)", async () => {
-    await act(() => {
-      render(<App />);
+        // Check for buttons 1 through 9
+        for (let i = 1; i <= 9; i++) {
+            const button = screen.getByRole("button", { name: i.toString() });
+            expect(button).toBeInTheDocument();
+        }
     });
 
-    // Check for buttons 1 through 9
-    for (let i = 1; i <= 9; i++) {
-      const button = screen.getByRole("button", { name: i.toString() });
-      expect(button).toBeInTheDocument();
-    }
-  });
-  
 });
