f9d1dc5938b38e6db49023cd3471f43c2750b47a
ilazarkov <ilazarkov@gmail.com>
2026-03-23 08:56:42 -0500

    fix tests
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>

--- src/__tests__/service.test.ts
+++ src/__tests__/service.test.ts
@@ -1,14 +1,20 @@
 import { Board } from "../model/Board";
 import { initBoard, SIZE } from "../service/Game";
+import { type TCellState } from "../model/Board";
 
-const notFalsy = (v: number | null) => v !== null && v > 0 && v < 10;
+const notFalsy = ({value, state}:{ value: number | null; state: TCellState }) =>
+  value !== null && value > 0 && value < 10;
 
 describe("Game Tests", () => {
   test("initialize board", () => {
-    const board = new Board()
+    const board = new Board();
     board.setBoard = initBoard();
-    expect(board.getBoard.length).toBe(SIZE);
-    expect(board.getBoard.every((row) => row.length === SIZE)).toBe(true);
-    expect(board.getBoard.every((row) => row.every(notFalsy))).toBe(true);
+    expect(Object.keys(board.getBoard).length).toBe(SIZE);
+    expect(
+      Object.values(board.getBoard).every((row) => row.length === SIZE),
+    ).toBe(true);
+    expect(
+      Object.values(board.getBoard).every((row) => row.every(notFalsy)),
+    ).toBe(true);
   });
 });
